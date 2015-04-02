10d82c6f8f417c9bad64a97ce92500292bb07953
Bernardo Lins <bernardolins28@gmail.com>
2015-04-02 20:16:47 -0300

    Implement get_subkey method.

--- src/bauer_yaml.cpp
+++ src/bauer_yaml.cpp
@@ -8,3 +8,8 @@ void bauer::tools::bauer_yaml::load_yaml(std::string path) {
 bauer::tools::bauer_yaml_node bauer::tools::bauer_yaml::get_key(std::string key) {
     return this->root_node[key];
 }
+
+bauer::tools::bauer_yaml_node bauer::tools::bauer_yaml::get_subkey_of(std::string key, std::string subkey) {
+    return this->root_node[key][subkey];
+      
+}
