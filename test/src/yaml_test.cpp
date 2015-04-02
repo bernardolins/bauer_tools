3b22947b259a0aa93a86d71377cd0cd79fa2a330
Bernardo Lins <bernardolins28@gmail.com>
2015-04-02 18:21:56 -0300

    Improved tests description.

--- test/src/yaml_test.cpp
+++ test/src/yaml_test.cpp
@@ -12,16 +12,26 @@ class yaml_test : public ::testing::Test {
     }
 };
 
-TEST_F(yaml_test, load_yaml) {
+TEST_F(yaml_test, should_load_yaml) {
     bauer::tools::bauer_yaml yaml;
     ASSERT_NO_THROW(yaml.load_yaml("files/test.yml"));
+}
+
+TEST_F(yaml_test, should_raise_exception_when_yml_not_found) {
+    bauer::tools::bauer_yaml yaml;
     ASSERT_ANY_THROW(yaml.load_yaml("files/nofile.yml"));
 }
 
-TEST_F(yaml_test, get_key) {
+TEST_F(yaml_test, should_get_value_of_key) {
     bauer::tools::bauer_yaml yaml;
     yaml.load_yaml("files/test.yml");
 
     ASSERT_EQ(yaml.get_key("key1").as<std::string>(), "value1");
 }
 
+TEST_F(yaml_test, should_get_not_defined_when_key_not_found) {
+    bauer::tools::bauer_yaml yaml;
+    yaml.load_yaml("files/test.yml");
+
+    ASSERT_FALSE(yaml.get_key("invalid_key").IsDefined());
+}
