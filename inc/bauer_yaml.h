a878ddd9a74bbe488ac5f89ced54f149e4fea268
Bernardo Lins <bernardolins28@gmail.com>
2015-04-02 13:14:07 -0300

    Add enumeration to yaml nodes.

--- inc/bauer_yaml.h
+++ inc/bauer_yaml.h
@@ -4,12 +4,19 @@
 #include "yaml-cpp/yaml.h"
 #include <iostream>
 
+enum yaml_nodes {
+    worker_list = "worker_list"
+};
+
 namespace bauer {
     namespace tools {
         class bauer_yaml {
             private:
+                YAML::Node node;
             public:
-                static bool load_yaml(std::string path);
+                bauer_yaml(std::string path);
+                //static bool load_yaml(std::string path);
+                void get_worker_list 
         };
     }
 }
