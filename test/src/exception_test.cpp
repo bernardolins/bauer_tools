e5d67a594af55894b0dd7bbdd38318bd95e60ccc
Bernardo Lins <bernardolins28@gmail.com>
2015-04-06 23:11:41 -0300

    Create test for exception.

---
+++ test/src/exception_test.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "bauer_exception.h"
+
+class exception_test : public ::testing::Test {
+    protected:
+    virtual void SetUp() {
+        
+    }
+
+    virtual void TearDown() {
+    
+    }
+
+    public:
+    static void throw_bauer_exception() {
+        throw bauer::tools::bauer_exception(); 
+            
+    }
+};
+
+TEST_F(exception_test, should_throw_bauer_exception) {
+   ASSERT_THROW(exception_test::throw_bauer_exception(), bauer::tools::bauer_exception);
+}
