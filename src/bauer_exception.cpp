6e634def272025e46be377f7190f8f58e5814ad4
Fausto Junqueira <faustoferjunqueira@gmail.com>
2015-04-30 20:13:14 -0300

    Bug fix bauer_exception.cpp

--- src/bauer_exception.cpp
+++ src/bauer_exception.cpp
@@ -1,5 +1,5 @@
 #include "bauer_exception.h"
 
 const char* bauer::bauer_exception::what() const throw() {
-    return "An unexpected error occurred. This can be a bug, please report at https://github.com/bernardolins/bauer_tools.";
+    return "An unexpected error occurred. This can be a bug, please report at https://github.com/frila/bauer_tools.";
 }
