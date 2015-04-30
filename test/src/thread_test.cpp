9195b791e21aa7a21a12b6d10436847a10685b24
Bernardo Lins <bernardolins28@gmail.com>
2015-04-30 20:43:47 -0300

    Create test class to thread.

---
+++ test/src/thread_test.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "bauer_thread.h"
+
+class thread_test : public ::testing::Test {
+    protected:
+    virtual void SetUp() {
+        
+    }
+
+    virtual void TearDown() {
+    
+    }
+};
+
+TEST_F(thread_test, should_start_thread) {
+	
+}
