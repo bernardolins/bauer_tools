bb4d375e98e407475666ec5b452a880fc238d9ad
Bernardo Lins <bernardolins28@gmail.com>
2015-04-30 20:41:43 -0300

    Add pid and pthread object.

--- inc/thread/bauer_thread.h
+++ inc/thread/bauer_thread.h
@@ -4,11 +4,15 @@
 namespace bauer {
 	class bauer_thread
 	{
+	private:
+		int pid;
+		pthread_t thread;
+		
 	public:
 		~bauer_thread();
-
 		void start();
 		void join();
-		virtual void exec(); 
+		virtual void exec();
+		int pid(); 
 	};
 }
