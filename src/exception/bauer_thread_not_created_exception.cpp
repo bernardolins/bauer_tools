7cbc59d37c25103632b5db046fe81d2f7e15b36c
Bernardo Lins <bernardolins28@gmail.com>
2015-05-05 20:04:53 -0300

    Fix wrong class name.

--- src/exception/bauer_thread_not_created_exception.cpp
+++ src/exception/bauer_thread_not_created_exception.cpp
@@ -1,5 +1,5 @@
 #include "bauer_thread_not_created_exception.h"
 
-const char* bauer::bauer_thread_not_found_exception::what() const throw() {
+const char* bauer::bauer_thread_not_created_exception::what() const throw() {
     return "An error occurred and the thread was not created.";
 }
